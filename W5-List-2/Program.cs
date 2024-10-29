f49f7ecca5ebf3fee30bb85e6c6259db5fce320f
İrem Rakıcı <iremrakici53@hotmail.com>
2024-10-29 14:43:21 +0300

    değişim

--- W5-List-2/Program.cs
+++ W5-List-2/Program.cs
@@ -2,33 +2,41 @@
 {
     static void Main()
     {
-        Console.WriteLine("Kahve menümüzü oluşturmak için tercihlerinize ihtiyacımız var!");
-        Console.WriteLine("Lütfen tercih ettiğiniz kahve için giriş yapınız.");
-        Console.Write("Kahve1:");
-        string coffee1 = Console.ReadLine();
-        Console.Write("Kahve2:");
-        string coffee2 = Console.ReadLine();
-        Console.Write("Kahve3:");
-        string coffee3 = Console.ReadLine();
-        Console.Write("Kahve4:");
-        string coffee4 = Console.ReadLine();
-        Console.Write("Kahve5:");
-        string coffee5 = Console.ReadLine();
 
-        List<string> list = new List<string>();
-        list.Add(coffee1);
-        list.Add(coffee2);
-        list.Add(coffee3);
-        list.Add(coffee4);
-        list.Add(coffee5);
 
-        Console.WriteLine("Girilen Kahve İsimleri:");
+        List<string> coffees = new List<string>();
 
-        int i = 1;
-        foreach (string menu in list)
+        Console.WriteLine("5 Adet Kahve İsmi Giriniz: ");
+
+        int coffeeCounter = 1;
+        for (int i = 0; i < 5; i++)
         {
-            Console.WriteLine($"{i} - {menu}");
-            i++;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write($"Kahve {coffeeCounter}: ");
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            string coffee = Console.ReadLine();
+
+            coffees.Add(coffee);
+            coffeeCounter++;
         }
+
+
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("Girilen Kahve İsimleri: ");
+
+        foreach (var coffee in coffees)
+        {
+            Console.WriteLine(coffee);
+        }
+
+
+
+
+        Console.ReadKey();
+
+
     }
 }
